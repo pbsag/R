6aa7e1b89734ea0f08abac88efcecbbe82ffdcd5
Kyle Ward <warddk@pbworld.com>
2017-03-07 21:10:00 -0500

    Change setup.bat to install to current directory

--- setup.bat
+++ setup.bat
@@ -1,1 +1,9 @@
-R-3.3.2-win.exe /SILENT
+rem this file must be run with admin rights
+
+rem install R to the current directory
+R-3.3.2-win.exe /SILENT /DIR=%cd%\R-3.3.2
+
+rem run the packages.R script
+R-3.3.2\bin\Rscript.exe packages.R
+
+pause
